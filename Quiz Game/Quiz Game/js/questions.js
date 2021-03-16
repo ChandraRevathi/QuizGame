8b312bdb7850ca3c18367dc55dbf3d71f2c84581
ChandraRevathi <72642599+ChandraRevathi@users.noreply.github.com>
2021-03-16 10:22:33 +0530

    Add files via upload

---
+++ Quiz Game/Quiz Game/js/questions.js
@@ -0,0 +1,72 @@
+// creating an array and passing the number, questions, options, and answers
+let questions = [
+    {
+    numb: 1,
+    question: "What is the full form HTML?",
+    answer: "Hyper Text Markup Language",
+    options: [
+      "Hyper Text Mark Language",
+      "Hyper Text Markup Language",
+      "Hyper Test Markup Language",
+      "Hybrid Text Markup Language"
+    ]
+  },
+    {
+    numb: 2,
+    question: "What is the tag used for paragraph?",
+    answer: "p",
+    options: [
+      "a",
+      "p",
+      "b",
+      "h"
+    ]
+  },
+    {
+    numb: 3,
+    question: "What is the tag used for horizontal line?",
+    answer: "hr",
+    options: [
+      "hr",
+      "line",
+      "liner",
+      "br"
+    ]
+  },
+    {
+    numb: 4,
+    question: "What is the tag used for breaking the line?",
+    answer: "br",
+    options: [
+      "br",
+      "bh",
+      "hr",
+      "b"
+    ]
+  },
+    {
+    numb: 5,
+    question: "What is the tag used for table row?",
+    answer: "tr",
+    options: [
+      "td",
+      "tr",
+      "th",
+      "ts"
+    ]
+  },
+  // you can uncomment the below codes and make duplicate as more as you want to add question
+  // but remember you need to give the numb value serialize like 1,2,3,5,6,7,8,9.....
+
+  //   {
+  //   numb: 6,
+  //   question: "Your Question is Here",
+  //   answer: "Correct answer of the question is here",
+  //   options: [
+  //     "Option 1",
+  //     "option 2",
+  //     "option 3",
+  //     "option 4"
+  //   ]
+  // },
+];
